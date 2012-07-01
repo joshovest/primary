71a83be8ca7a1191a9b297f0a26fae8999333a54
Josh West <joshovest@gmail.com>
2012-06-30 22:09:06 -0600

    Fixed max birthday issue

--- app/controllers/primary_children_controller.rb
+++ app/controllers/primary_children_controller.rb
@@ -9,7 +9,7 @@ class PrimaryChildrenController < ApplicationController
     sort = params[:sort] == "class" ? "primary_class_id,birthday,last_name,first_name" : "last_name,first_name,birthday"
     max_age = 12
     min_age = 4
-    max_birthday = Time.new(Time.now.year-max_age, 12, 31)
+    max_birthday = Time.new(Time.now.year-max_age, 1, 1)
     min_birthday = Time.new(Time.now.year-min_age, 12, 31)
     
     @primary_children = PrimaryChild.where(:birthday => (max_birthday..min_birthday)).order(sort)
